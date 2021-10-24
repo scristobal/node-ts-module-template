2658d2c69512d82f996ecfeb6d82f752b3b14c9c
Samuel Cristobal <sc@databeacon.aero>
2021-10-24 09:21:58 +0200

    better prettier

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,15 @@ export default {
     verbose: true,
     moduleNameMapper: {
         '^@App/(.*)$': '<rootDir>/src/$1',
-        '^lib/(.*)$': '<rootDir>/common/$1',
+        '^lib/(.*)$': '<rootDir>/common/$1'
     },
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     extensionsToTreatAsEsm: ['.ts'],
     globals: {
         'ts-jest': {
-            useESM: true,
-        },
+            useESM: true
+        }
     },
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
-}
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/']
+};
